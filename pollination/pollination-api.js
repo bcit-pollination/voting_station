b3d1bc3e7bad2e9b3009aa909140a33649b5bad4
BGetz <bgetz@ascendentgroup.com>
2021-03-09 11:57:42 -0700

    Added a couple more endpoints and added authorization header checking

--- pollination/pollination-api.js
+++ pollination/pollination-api.js
@@ -3,13 +3,15 @@ const axios = require('axios').default;
 const methods = { "GET": "get", "POST": "post", "PUT": "put", "DELETE": "delete" };
 
 axios.defaults.baseURL = 'http://pollination.live/api';
+axios.defaults.headers.common['Authorization'] = '';
 
 const urlLogin = '/user/login';
-const urlUser = '/user'
-const urlOrg = '/org'
-const urlVerifierPass = '/org/verifier_password'
-
-axios.defaults.baseURL = urlBase;
+const urlUser = '/user';
+const urlOrg = '/org';
+const urlVerifierPass = '/org/verifier_password';
+const urlElections = '/org/elections';
+const urlElectionVotes = '/org/election/votes';
+const urlElectionDownload = '/org/election/download';
 
 
 export function login(username, password) {
@@ -24,8 +26,16 @@ export function login(username, password) {
     })
 }
 
+export function getVerifierPassword(org_id) {
+    return axiosRequest(methods.POST, urlVerifierPass, { "org_id": org_id });
+}
+
 export function logout() {
     axios.defaults.headers.common['Authorization'] = '';
+}
+
+export function electionDownload() {
+    return axiosRequest(methods.POST, urlVerifierPass, { "org_id": org_id });
 }
 
 function axiosRequest(method, url, data) {
@@ -38,6 +48,11 @@ function axiosRequest(method, url, data) {
         requestObject['data'] = data;
 
     return new Promise((resolve, reject) => {
+        if (!axios.defaults.headers.common['Authorization']) {
+            console.log("Missing authorization token, login first");
+            reject();
+        }
+
         axios(requestObject)
             .then(resp => {
                 console.log(resp)
