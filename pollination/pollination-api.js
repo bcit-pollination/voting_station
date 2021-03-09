483c7cb08246b070efe40d5a4772433db7dd9dd3
BGetz <bgetz@ascendentgroup.com>
2021-03-09 11:12:04 -0700

    Added enums for endpoints changed internal functions to use single internal request function

--- pollination/pollination-api.js
+++ pollination/pollination-api.js
@@ -1,24 +1,28 @@
 const axios = require('axios').default;
 
-const urlBase = 'http://pollination.live/api';
-const urlLogin = urlRoot + '/user/login';
+const methods = { "GET": "get", "POST": "post", "PUT": "put", "DELETE": "delete" };
+
+axios.defaults.baseURL = 'http://pollination.live/api';
+
+const urlLogin = '/user/login';
+const urlUser = '/user'
+const urlOrg = '/org'
+const urlVerifierPass = '/org/verifier_password'
 
 axios.defaults.baseURL = urlBase;
 
+
 export function login(username, password) {
     return new Promise((resolve, reject) => {
-        axios.post(urlLogin, { username: username, password: password })
+        axiosRequest(methods.POST, urlLogin, { username: username, password: password })
             .then(resp => {
-                console.log(resp)
                 axios.defaults.headers.common['Authorization'] = "Bearer " + resp['jwt_token'];
             })
             .catch(err => {
-                console.log(err)
                 reject(err);
             });
     })
 }
-
 
 export function logout() {
     axios.defaults.headers.common['Authorization'] = '';
@@ -33,5 +37,15 @@ function axiosRequest(method, url, data) {
     if (data)
         requestObject['data'] = data;
 
-    axios(requestObject);
+    return new Promise((resolve, reject) => {
+        axios(requestObject)
+            .then(resp => {
+                console.log(resp)
+                resolve(resp);
+            })
+            .catch(err => {
+                console.log(err)
+                reject(err);
+            });
+    });
 }
