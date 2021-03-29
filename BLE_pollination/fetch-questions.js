960b466c681aa774d4427ca2e30a96badd8be5f5
BGetz <bgetz@ascendentgroup.com>
2021-03-29 10:19:28 -0600

    Remove unused/empty files from ble folder, moved utils folder to top level

--- BLE_pollination/fetch-questions.js
+++ BLE_pollination/fetch-questions.js
@@ -1,56 +1,57 @@
 function fetchQuestionViaMongoConnection() {
-
-
     //Mongo Connection
-    const MongoClient = require('mongodb').MongoClient
+    const MongoClient = require("mongodb").MongoClient;
     // default Mongo connection string
-    const mongoHost = '127.0.0.1';
-    const userName = ''
-    const userPassword = ''
-    const mongoPort = 27017
-    const dbName = 'test_mongo_schema_creation'
-    const collectionName = 'questions'
+    const mongoHost = "127.0.0.1";
+    const userName = "";
+    const userPassword = "";
+    const mongoPort = 27017;
+    const dbName = "test_mongo_schema_creation";
+    const collectionName = "questions";
 
     // fetch questions from DB
     // MongoClient.connect(`mongodb://${userName}:${userPassword}@${mongoHost}:${mongoPort}/`
-    MongoClient.connect(`mongodb://${mongoHost}:${mongoPort}/`
-        , function (err, db) {
+    MongoClient.connect(
+        `mongodb://${mongoHost}:${mongoPort}/`,
+        function(err, db) {
             if (err) throw err;
             var dbo = db.db(`${dbName}`);
-            dbo.collection(collectionName).find({}).toArray(function (err, result) {
-                if (err) throw err;
-                // console.log(result);
-                db.close();
-                return result
-            });
-        });
+            dbo
+                .collection(collectionName)
+                .find({})
+                .toArray(function(err, result) {
+                    if (err) throw err;
+                    // console.log(result);
+                    db.close();
+                    return result;
+                });
+        }
+    );
 }
-
 
 async function fetchQuestionViaAPI() {
     const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
     // const xhr = new XMLHttpRequest();
 
-    let loadQuestionApi = 'http://localhost:3000/getQuestions'
+    let loadQuestionApi = "http://localhost:3000/getQuestions";
 
-//    let getQuestionPromise =
-    return new Promise(async function (resolve, reject) {
+    //    let getQuestionPromise =
+    return new Promise(async function(resolve, reject) {
         let xhttp = new XMLHttpRequest();
-        let response = ''
+        let response = "";
 
         xhttp.open("GET", loadQuestionApi, true);
         xhttp.send();
-        xhttp.onreadystatechange = function () {
+        xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-
                 response = this.responseText;
 
                 // console.log(response)
-                resolve(response)
+                resolve(response);
             }
-            return response
-        }
-    })
+            return response;
+        };
+    });
     // return await getQuestionPromise.then(question => {
     // //    console.log(question)
     //     return question
@@ -61,30 +62,29 @@ async function checkVotabliy() {
     const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
     // const xhr = new XMLHttpRequest();
 
-    let checkVotabliyAPI = 'http://localhost:3000/CheckVotability'
+    let checkVotabliyAPI = "http://localhost:3000/CheckVotability";
 
-//    let getQuestionPromise =
-    return new Promise(async function (resolve, reject) {
+    //    let getQuestionPromise =
+    return new Promise(async function(resolve, reject) {
         let xhttp = new XMLHttpRequest();
-        let response = ''
+        let response = "";
 
         xhttp.open("GET", checkVotabliyAPI, true);
         xhttp.send();
-        xhttp.onreadystatechange = function () {
+        xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-
                 response = this.responseText;
 
                 // console.log(response)
-                resolve(response)
+                resolve(response);
             }
-            return response
-        }
-    })
+            return response;
+        };
+    });
 }
 
 module.exports = {
     fetchQuestionViaAPI: fetchQuestionViaAPI,
-    fetchQuestionViaMongoConnection :fetchQuestionViaMongoConnection,
-    checkVotabliy : checkVotabliy
-}
+    fetchQuestionViaMongoConnection: fetchQuestionViaMongoConnection,
+    checkVotabliy: checkVotabliy,
+};
