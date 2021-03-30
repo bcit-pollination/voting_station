68f803d580bd95c8a8b712553359503f7a20aad9
Hans Sy <55111820+hanssy130@users.noreply.github.com>
2021-03-30 16:13:16 -0700

    fix radio/checkbox

--- touchscreen_electron/pages/js/voting.js
+++ touchscreen_electron/pages/js/voting.js
@@ -150,7 +150,7 @@ function loadPoll(questJSON) {
             label.style.fontSize = "1em";
             label.appendChild(document.createTextNode(questOps[j].option_description));
             questDiv.appendChild(label);
-            if (questArray[j].max_selection_count == 1 && questArray[j].min_selection_count == 1) {
+            if (questArray[i].max_selection_count == 1 && questArray[i].min_selection_count == 1) {
                 inpt.type = "radio";
             } else {
                 inpt.type = "checkbox";
